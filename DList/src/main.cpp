fb201c91722377883f01cf426950198e9028a8a7
tchtrong <tchtrong@apcs.vn>
2022-01-16 03:42:53 +0700

    Add bidirectional_iterator test

--- DList/src/main.cpp
+++ DList/src/main.cpp
@@ -1,5 +1,15 @@
 import dlist;
 
+#include <iterator>
+
 int main() {
-	MyLib::DList<int> a;
+	using MyLib::DList;
+	using iterator = MyLib::DList<int>::iterator;
+	using const_iterator = MyLib::DList<int>::const_iterator;
+
+	// Check if iterator is a bidirectional_iterator
+	static_assert(std::bidirectional_iterator<iterator>);
+	static_assert(std::bidirectional_iterator<const_iterator>);
+
+
 }
